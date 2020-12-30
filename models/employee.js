07db4a28cc8b4bf83b13a0dd895da2a211fed10d
Nitheesh KR <nitheeshkr115@gmail.com>
2020-12-30 09:53:03 +0530

    removed unwanted files

--- models/employee.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const employeeSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  designation: {type: String, required: true},
-})
-
-module.exports = mongoose.model('Employee', employeeSchema);
